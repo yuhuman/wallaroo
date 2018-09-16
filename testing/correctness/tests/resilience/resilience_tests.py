87b0f616c61c1ce0a4318ee29ae31c68601daf40
John Mumm <jtfmumm@gmail.com>
2018-09-16 11:42:49 +0200

    ..

--- testing/correctness/tests/resilience/resilience_tests.py
+++ testing/correctness/tests/resilience/resilience_tests.py
@@ -18,85 +18,85 @@ from resilience import (Crash,
 # Test spec(s)
 #############
 
-def test_auto0():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(1)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto0():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Grow(1)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto1():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(1), Wait(2), Grow(4)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto1():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Grow(1), Wait(2), Grow(4)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto2():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(1), Wait(2), Shrink(1)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto2():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Grow(1), Wait(2), Shrink(1)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto3():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(1), Wait(2), Shrink(4)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto3():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Grow(1), Wait(2), Shrink(4)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto4():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(4)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto4():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Grow(4)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto5():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(4), Wait(2), Grow(1)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto5():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Grow(4), Wait(2), Grow(1)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto6():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(4), Wait(2), Shrink(1)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto6():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Grow(4), Wait(2), Shrink(1)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto7():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(4), Wait(2), Shrink(4)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto7():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Grow(4), Wait(2), Shrink(4)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto8():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(1)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto8():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Shrink(1)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto9():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(1), Wait(2), Shrink(4)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto9():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Shrink(1), Wait(2), Shrink(4)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto10():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(1), Wait(2), Grow(1)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto10():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Shrink(1), Wait(2), Grow(1)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto11():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(1), Wait(2), Grow(4)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto11():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Shrink(1), Wait(2), Grow(4)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto12():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(4)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto12():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Shrink(4)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto13():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(4), Wait(2), Shrink(1)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto13():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Shrink(4), Wait(2), Shrink(1)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto14():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(4), Wait(2), Grow(1)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto14():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Shrink(4), Wait(2), Grow(1)]
+#     _test_resilience(command, ops, validate_output=True)
 
-def test_auto15():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(4), Wait(2), Grow(4)]
-    _test_resilience(command, ops, validate_output=True)
+# def test_auto15():
+#     command = 'multi_partition_detector --depth 1'
+#     ops = [Shrink(4), Wait(2), Grow(4)]
+#     _test_resilience(command, ops, validate_output=True)
 
 
 # some fixed tests:
